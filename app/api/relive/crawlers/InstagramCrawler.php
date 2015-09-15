d59b7ea65a7f6f045ede2e9910dbba84f3db7e5a
ianngiaw <ian@u.nus.edu>
2015-09-16 00:25:58 +0800

    added instagram crawl

--- app/api/relive/crawlers/InstagramCrawler.php
+++ app/api/relive/crawlers/InstagramCrawler.php
@@ -3,10 +3,12 @@
 namespace relive\Crawlers;
 
 use MetzWeb\Instagram\Instagram;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class InstagramCrawler extends \relive\Crawlers\Crawler {
 
     private $instagram;
+    private $provider;
 
 	private function __construct(){
         $this->instagram = new Instagram(array(
@@ -16,6 +18,7 @@ class InstagramCrawler extends \relive\Crawlers\Crawler {
         ));
         $access_token = getenv('INSTAGRAM_ACCESS_TOKEN');
         $this->instagram->setAccessToken($access_token);
+        $this->provider = \relive\models\Provider::firstOrCreate(['providerName'=>'instagram','providerSite'=>'https://www.instagram.com/']);
 	}
 
     public static function getInstance() {
@@ -25,20 +28,104 @@ class InstagramCrawler extends \relive\Crawlers\Crawler {
         return $instance;
     }
 
-	public function crawl($keyword){
+	public function recentCrawl($startTime, $event, $keyword){
         $response = $this->instagram->searchTags($keyword);
-
         $tags = $response->data;
         if (count($tags) != 0) {
             $tag = $tags[0]->name;
             $media = $this->instagram->getTagMedia($tag, 90);
-            echo json_encode($media);
+            $count = 2;
             while (count($media->data)) {
+                foreach ($media->data as $instaPost) {
+                    if ($instaPost->created_time > $startTime - 600) {
+                        $this->createPost($event, $instaPost);
+                    } else {
+                        return;
+                    }
+                }
+                if ($count <= 0)
+                    return;
+                $count--;
                 $media = $this->instagram->pagination($media, 90);                
-                echo json_encode($media);
             }
-        } else {
-            echo "error";
         }
 	}
+
+    public function initialCrawl($event, $keyword){
+        $response = $this->instagram->searchTags($keyword);
+        $tags = $response->data;
+        if (count($tags) != 0) {
+            $tag = $tags[0]->name;
+            $media = $this->instagram->getTagMedia($tag, 90);
+            $count = 5;
+            while (count($media->data)) {
+                foreach ($media->data as $instaPost) {
+                    $this->createPost($event, $instaPost);
+                }
+                if ($count <= 0)
+                    return;
+                $count--;
+                $media = $this->instagram->pagination($media, 90);                
+            }
+        }
+    }
+
+    private function createPost($event, $instaPost) {
+        if ($instaPost->type === "image") {
+            try {
+                return \relive\models\Post::where('postURL', '=', $instaPost->link)->firstOrFail();
+            } catch (ModelNotFoundException $e) {
+                $datetime = new \DateTime();
+                $datetime->setTimestamp($instaPost->created_time);
+                $post = \relive\models\Post::firstOrCreate([
+                    'datetime'=>$datetime,
+                    'postURL'=>$instaPost->link,
+                    'author'=>$instaPost->user->username,
+                    'caption'=>$instaPost->caption->text,
+                    'provider_id'=>$this->provider->provider_id
+                ]);
+                if (isset($instaPost->images)) {
+                    $this->saveImageUrls($post, $instaPost->images);
+                }
+                $relationship = \relive\models\PostEventRelationship::firstOrCreate([
+                    'event_id'=>$event->event_id,
+                    'post_id'=>$post->post_id,
+                    'isFiltered'=>0
+                ]);
+                return $post;
+            }
+        }
+    }
+
+    private function saveImageUrls($post, $instaImages) {
+        $media = \relive\models\Media::create(['post_id'=>$post->post_id, 'type'=>'photo']);
+        $media_id = $media->media_id;
+        if (isset($instaImages->low_resolution)) {
+            $media_url = \relive\models\MediaURL::firstOrCreate([
+                'media_id'=>$media_id,
+                'mediaURL'=>$instaImages->low_resolution->url,
+                'width'=>$instaImages->low_resolution->width,
+                'height'=>$instaImages->low_resolution->height,
+                'sizes'=>'low_resolution'
+            ]);
+        }
+        if (isset($instaImages->thumbnail)) {
+            $media_url = \relive\models\MediaURL::firstOrCreate([
+                'media_id'=>$media_id,
+                'mediaURL'=>$instaImages->thumbnail->url,
+                'width'=>$instaImages->thumbnail->width,
+                'height'=>$instaImages->thumbnail->height,
+                'sizes'=>'thumbnail'
+            ]);
+        }
+        if (isset($instaImages->standard_resolution)) {
+            $media_url = \relive\models\MediaURL::firstOrCreate([
+                'media_id'=>$media_id,
+                'mediaURL'=>$instaImages->standard_resolution->url,
+                'width'=>$instaImages->standard_resolution->width,
+                'height'=>$instaImages->standard_resolution->height,
+                'sizes'=>'standard_resolution'
+            ]);
+        }
+    }
 }
