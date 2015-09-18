c4dd0b7575152917cf0b92f28b1f5faa0a50ae8d
Quan Yang <quanyang@live.com>
2015-09-18 15:29:39 +0800

    Attempts to use fork for initial crawl

--- app/api/relive/controllers/EventController.php
+++ app/api/relive/controllers/EventController.php
@@ -157,9 +157,12 @@ class EventController extends Controller {
 					$eventhashtagrelationship = \relive\models\EventHashtagRelationship::firstOrCreate(['event_id'=>$event->event_id, 'hashtag_id' => $hashtag->hashtag_id]);
 				}
 			}
-			echo json_encode($event, JSON_UNESCAPED_SLASHES);
-      $crawler = \relive\Crawlers\AsyncCreationCrawler($event);
-      $crawler->start();
+      $pid = \pcntl_fork();
+      if($pid) {
+        echo json_encode($event, JSON_UNESCAPED_SLASHES);
+      } else {
+        \relive\Crawlers\CreationCrawler::initialCrawl($event);
+      }
 
 		} catch (\Exception $e) {
 			$app->render(500, ['Status' => 'An error occurred.' ]);
--- app/api/relive/crawlers/CreationCrawler.php
+++ app/api/relive/crawlers/CreationCrawler.php
@@ -19,24 +19,4 @@ class CreationCrawler extends \relive\Crawlers\Crawler {
 				'event_id'=>$event->event_id
 		]);
 	}
-
-}
-
-class AsyncCreationCrawler extends CreationCrawler {
-	public $event;
-
-    public function __construct($event) {
-        $this->event = $event;
-    }
-
-	public function run() {
-		if (($event = $this->event)) {
-            /*
-             * If a large amount of data is being requested, you might want to
-             * fsockopen and read using usleep in between reads
-             */
-            $this->initialCrawl($event);
-        } 
-
-	}
 }
