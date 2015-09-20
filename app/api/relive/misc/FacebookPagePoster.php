dfc31d96ee21d1da1e217a2b1fa6ff7ff83592bc
ianngiaw <ian@u.nus.edu>
2015-09-20 15:06:28 +0800

    use rawurlencode instead

--- app/api/relive/misc/FacebookPagePoster.php
+++ app/api/relive/misc/FacebookPagePoster.php
@@ -19,7 +19,7 @@ if (isset($event_id)) {
 
 		$fbPageId = getenv('FACEBOOK_PAGE_ID');
 
-		$sourceLink = "https://relive.space/#!/event.php?id=" . $event->event_id . "&name=" . htmlentities($event->eventName);
+		$sourceLink = "https://relive.space/#!/event.php?id=" . $event->event_id . "&name=" . rawurlencode($event->eventName);
 		$fbPostMessage = "The event \"" . $event->eventName . "\" was just added to relive.space!\n\n" . $sourceLink;
 
 		$response = $fb->post('/' . $fbPageId . '/feed', ['message' => $fbPostMessage]);
