758bd9e9ea21036661e970642e5b23b071f292f0
Quan Yang <quanyang@live.com>
2015-09-22 13:43:18 +0800

    Report post count including past reports

--- app/api/moderate.php
+++ app/api/moderate.php
@@ -52,6 +52,6 @@ if (!isset($_SESSION['login'])) {
 	include './relive/templates/login.php';
 } else {
 	$unpublishedEvents = \relive\models\Event::where('isPublished','=','0')->get();
-	$reportedPosts = \relive\models\Post::join('reports','posts.post_id','=','reports.post_id')->groupBy('posts.post_id')->selectRaw('*,count(*) as `count`	')->orderBy('datetime','desc')->get();
+	$reportedPosts = \relive\models\Post::join('reports','posts.post_id','=','reports.post_id')->groupBy('posts.post_id')->selectRaw('*,(select count(*) from reports c where c.post_id = posts.post_id) as `count`	')->orderBy('datetime','desc')->get();
 	include './relive/templates/moderate.php';
 }
